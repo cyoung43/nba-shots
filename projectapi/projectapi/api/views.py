80836810922d894fb4e0756a0d0ee03c0e88f5a7
dallin35 <56549866+dallin35@users.noreply.github.com>
2020-04-21 20:39:49 -0600

    please work

--- projectapi/projectapi/api/views.py
+++ projectapi/projectapi/api/views.py
@@ -45,40 +45,43 @@ def runML(scoring_uri, key, input):
 
 @csrf_exempt
 @api_view(['GET', 'POST', ])
-def getExpectedAvgDonationCorona(request, fromMethod=False): # Corona Virus Related
+def getShotPrediction(request, fromMethod=False): # Corona Virus Related
     
     
-    donationsURL = 'https://ussouthcentral.services.azureml.net/workspaces/c76b9f0e8066401db73b32ba2ea74ed9/services/4172a5aa47424881a36753d3d79b42f2/execute?api-version=2.0&details=true'
-    donationsKey = 'DIKaCG+NIpUDVk3wCD34yZ6JvZHdZM5+0JWmPFrQNiWO7NSBc7zVjSnWbOyL2ZSWNE4xe8NWMRau4arzs6cCzg=='
+    shotsURL = 'https://ussouthcentral.services.azureml.net/workspaces/e7626090de274eec971d136acbc67e67/services/ed1ee599e573413bb3961627ee44eb89/execute?api-version=2.0&details=true'
+    shotsKey = 'H1hkxnUbil+UYJmXPQ2bzA7BB6n5n6gCdt9SLE+jAzgxuITlFvHIBw/hpqKF7FnTVigsLH1RnE/5ELMWtgL22g=='
 
 
     stuff = json.loads(request.body)
     
 
-    auto_fb_post_mode = stuff['auto_fb_post_mode']
-    currencycode = stuff['currencycode']
-    goal = stuff['goal']
-    days_active = stuff['days_active']
-    title = stuff['title']
-    description = stuff['description']
-    social_share_total = stuff['social_share_total']
-    location_country = stuff['location_country']
-    average_donation = '0000'
+    location = stuff['LOCATION']
+    shot_number = stuff['SHOT_NUMBER']
+    period = stuff['PERIOD']
+    shot_clock = stuff['SHOT_CLOCK']
+    dribbles = stuff['DRIBBLES']
+    shot_distance = stuff['SHOT_DISTANCE']
+    pts_type = stuff['PTS_TYPE']
+    close_def_dist = stuff['CLOSE_DEF_DIST']
+    game_clock = stuff['GAME_CLOCK']
+    fg = stuff['FG']
+    experience = stuff['EXPERIENCE']
+
 
     
 
-    donationsInputs = {
-        "ColumnNames": ["auto_fb_post_mode", "currencycode", "goal", "days_active", "title", "description", "social_share_total", "location_country", "average_donation"],
-        "Values": [[auto_fb_post_mode, currencycode, goal, days_active, title, description, social_share_total, location_country, average_donation], ]
+    shotsInputs = {
+        "ColumnNames": ["location", "shot_number", "period", "shot_clock", "dribbles", "shot_distance", "pts_type", "close_def_dist", "game_clock", "fg", "experience"],
+        "Values": [[location, shot_number, period, shot_clock, dribbles, shot_distance, pts_type, close_def_dist, game_clock, fg, experience], ]
     }
 
-    expected_avg_donation = str(
-        runML(donationsURL, donationsKey, donationsInputs))
+    shots_prediction = str(
+        runML(shotURL, shotKey, shotInputs))
 
     if fromMethod:
-        return expected_avg_donation
+        return shots_prediction
     else:
-        return Response({"result": expected_avg_donation})
+        return Response({"result": shots_prediction})
         
     #render(request, "src\campaign-calculator-container.js", {"result": expected_avg_donation})
 
@@ -86,110 +89,21 @@ def getExpectedAvgDonationCorona(request, fromMethod=False): # Corona Virus Rela
 
 @csrf_exempt
 @api_view(['GET', 'POST', ])
-def getExpectedNumDonorsCorona(request): # Corona Virus Related
-    donorsURL = 'https://ussouthcentral.services.azureml.net/workspaces/c76b9f0e8066401db73b32ba2ea74ed9/services/155f03648c3847659a35ce4734227129/execute?api-version=2.0&details=true'
-    donorsKey = 'aLl69DHFyVdkWJj4b2GSks3eT/FmeCliMPADx0VCRi2rLy7pMshJKZodPVnnUsWVktXNWVtDf/IIwKyVgOitaw=='
+def getPlayerRecommendation(request): # Corona Virus Related
+    playersURL = 'https://ussouthcentral.services.azureml.net/workspaces/e7626090de274eec971d136acbc67e67/services/b85c3fe5a9814626bec6d9bcc3b91a27/execute?api-version=2.0&details=true'
+    playersKey = 'hFY+UlW9/nPCsFtcvOGukx2VEEc/7C8l1zpq/K9YSpc2+8RwJA+sUKvzxY5ys1BcnhoP1TpOQhPPCXs5fdatKg=='
 
     stuff = json.loads(request.body)
 
-    campaign_id = '0000'
-    auto_fb_post_mode = stuff['auto_fb_post_mode']
-    currencycode = stuff['currencycode']
-    goal = stuff['goal']
-    donators = '0000'
-    days_active = stuff['days_active']
-    title = stuff['title']
-    description = stuff['description']
-    social_share_total = stuff['social_share_total']
-    location_country = stuff['location_country']
-    average_donation = '0000'
-    expected_avg_donation = stuff['expected_avg_donation']#getExpectedAvgDonation(request.body, True)
+    full_name = stuff['Full_Name']
 
-    donorsInputs = {
-        "ColumnNames": ["campaign_id", "auto_fb_post_mode", "currencycode", "goal", "donators", "days_active", "title", "description", "social_share_total", "location_country", "average_donation"],
-        "Values": [[campaign_id, auto_fb_post_mode, currencycode, goal, donators, days_active, title, description, social_share_total, location_country, expected_avg_donation], ]
+    playersInputs = {
+        "ColumnNames": ["full_name"],
+        "Values": [[full_name], ]
     }
 
     expected_num_donors = str(
-        round(float(runML(donorsURL, donorsKey, donorsInputs))))
-
-    return Response({"result": {"expected_donors": expected_num_donors, "expected_donation": expected_avg_donation}})
-    
-    #render(request, "campaign-calculator-container.js", {"result": {"expected_donors": expected_num_donors, "expected_donation": expected_avg_donation}})
-
-
-
-###################### NON-CORONA VIRUS PREDICTIONS ##########################
-@csrf_exempt
-@api_view(['GET', 'POST', ])
-def getExpectedAvgDonationRegular(request, fromMethod=False): # Corona Virus Related
-    
-    
-    donationsURL = 'https://ussouthcentral.services.azureml.net/workspaces/c76b9f0e8066401db73b32ba2ea74ed9/services/ffff27b371dd45a1a19ce723cf8f6fac/execute?api-version=2.0&details=true'
-    donationsKey = 'qNxK76DRGm0aYMUh1U8tV1uU7uKGPItqCANUrFrDaTgd8s+Bz76PO/hXRNWWIO7UwF4Pj5tqJqP82FpQxtDM/w=='
-
-
-    stuff = json.loads(request.body)
-    
-
-    auto_fb_post_mode = stuff['auto_fb_post_mode']
-    currencycode = stuff['currencycode']
-    goal = stuff['goal']
-    days_active = stuff['days_active']
-    title = stuff['title']
-    description = stuff['description']
-    social_share_total = stuff['social_share_total']
-    location_country = stuff['location_country']
-    average_donation = '0000'
-
-    
-
-    donationsInputs = {
-        "ColumnNames": ["auto_fb_post_mode", "currencycode", "goal", "days_active", "title", "description", "social_share_total", "location_country", "average_donation"],
-        "Values": [[auto_fb_post_mode, currencycode, goal, days_active, title, description, social_share_total, location_country, average_donation], ]
-    }
-
-    expected_avg_donation = str(
-        runML(donationsURL, donationsKey, donationsInputs))
-
-    print('AZUREML TEST: ', expected_avg_donation)
-    if fromMethod:
-        return expected_avg_donation
-    else:
-        return Response({"result": expected_avg_donation})
-        
-    #render(request, "src\campaign-calculator-container.js", {"result": expected_avg_donation})
-
-# Relies on the getExpectedAvgDonation method above
-
-@csrf_exempt
-@api_view(['GET', 'POST', ])
-def getExpectedNumDonorsRegular(request): # Corona Virus Related
-    donorsURL = 'https://ussouthcentral.services.azureml.net/workspaces/c76b9f0e8066401db73b32ba2ea74ed9/services/21464a08fbfc4aedb91a0df543ec3129/execute?api-version=2.0&details=true'
-    donorsKey = 'W1kzIjC5W1aYs7Yu9hqjWBAN39UMXPJkg2IZuKn41d4tHHNHvgzT/i64GRaBw+oC7W9OC3KZyEMExF0of6S3dw=='
-
-    stuff = json.loads(request.body)
-
-    campaign_id = '0000'
-    auto_fb_post_mode = stuff['auto_fb_post_mode']
-    currencycode = stuff['currencycode']
-    goal = stuff['goal']
-    donators = '0000'
-    days_active = stuff['days_active']
-    title = stuff['title']
-    description = stuff['description']
-    social_share_total = stuff['social_share_total']
-    location_country = stuff['location_country']
-    average_donation = '0000'
-    expected_avg_donation = stuff['expected_avg_donation']#getExpectedAvgDonation(request.body, True)
-
-    donorsInputs = {
-        "ColumnNames": ["campaign_id", "auto_fb_post_mode", "currencycode", "goal", "donators", "days_active", "title", "description", "social_share_total", "location_country", "average_donation"],
-        "Values": [[campaign_id, auto_fb_post_mode, currencycode, goal, donators, days_active, title, description, social_share_total, location_country, expected_avg_donation], ]
-    }
-
-    expected_num_donors = str(
-        round(float(runML(donorsURL, donorsKey, donorsInputs))))
+        round(float(runML(playersURL, playersKey, playersInputs))))
 
     return Response({"result": {"expected_donors": expected_num_donors, "expected_donation": expected_avg_donation}})
     
